ff604193c14d417cd3c82fb6fbd3740d9842340b
Ali Naqi Al-Musawi <alinaqi2000@gmail.com>
2021-03-24 01:56:57 +0500

    Initial Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Ali Naqi Al-Musawi Here!</h3>
+      <h4>My site name is {process.env.REACT_APP_SITE_NAME}</h4>
     </div>
   );
 }
